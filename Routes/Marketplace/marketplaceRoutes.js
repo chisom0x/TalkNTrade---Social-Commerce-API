b4c58d342a02fb348c383a8396f44d21b930b215
chisomOx <onyenankiekelvin@gmail.com>
2024-01-30 05:06:59 +0100

    modified:   Routes/Marketplace/marketplaceRoutes.js

--- Routes/Marketplace/marketplaceRoutes.js
+++ Routes/Marketplace/marketplaceRoutes.js
@@ -3,12 +3,8 @@ const router = express.Router()
 const storesController = require('../../Controller/Marketplace/storeController')
 const filterController = require('../../Controller/Marketplace/filterController')
 const productController = require ('../../Controller/Marketplace/productController')
-//const searchController = require('../../Controller/Marketplace/searchController')
-//const middleware = require('../../Middlewares/checkStoreOwnerhip')
-//const multer = require('multer')
-//const storage = multer.memoryStorage()
 
-// store routes
+
 router.post('/create-store', storesController.createStore)
 
 router
@@ -23,8 +19,6 @@ router
 .route('/edit-store-info/:id')
 .patch(storesController.editStoreInfo)
 
-// product filter routes
-
 router
 .route('/add-category')
 .post(filterController.addCategory)
@@ -32,9 +26,6 @@ router
 router
 .route('/get-categories')
 .get(filterController.getAllCategories)
-
-
-// product routes
 
 router.post('/add-product', productController.addProduct)
 
@@ -58,38 +49,9 @@ router
 .route('/delete-product/:productId')
 .delete(productController.deleteProduct)
 
-// reviews routes
-
 router
 .route('/products/:productId/add-review')
 .post(productController.addReview)
-
-
-// chat system
-
-// router
-// .route('/chat/send-message/:productId')
-// .post(chatController.sendMessage)
-
-// router.post('/chat/send-image/:productId', upload.single('image'), chatController.sendImage)
-
-// router
-// .route('/conversations/user')
-// .get(chatController.getUserConversations)
-
-// router 
-// .route('/conversations/:conversationId')
-// .get(chatController.getConversation)
-
-// router
-// .route('/chat/delete-message/:messageId')
-// .delete(chatController.deleteMessage)
-
-// router
-// .route('/chat/delete-image/:messageId')
-// .delete(chatController.deleteImage)
-
-
 
 
 module.exports = router
